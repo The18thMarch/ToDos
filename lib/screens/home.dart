34f5e7dc9c2da41692e48cc4984a4db098c9d380
18thMarch <the18thmarchcode@gmail.com>
2026-09-20 20:22:40 +0545

    Second Push

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
+import 'package:todo_app/widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -27,6 +28,19 @@ class Home extends StatelessWidget {
                       ),
                     ),
                   ),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
+                  TodoItem(),
                 ],
               ),
             ),
